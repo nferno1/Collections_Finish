ec8998f08131444d70c5cd9734c78bc0493e017d
nferno1 <nferno1@mail.ru>
2022-09-09 16:32:27 +0700

    Cats ArrayList example

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) {
@@ -11,9 +13,21 @@ public class Main {
         cats[3] = new Cat("Котяра");
 
 
-        cats[1] = null;
+        //cats[1] = null;
 
         System.out.println(Arrays.toString(cats));
+
+
+        ArrayList<Cat> catsList = new ArrayList();
+        for (Cat cat : cats){
+            catsList.add(cat);
+
+        }
+        catsList.add(new Cat("Кошара"));
+
+
+
+        System.out.println(catsList.toString());
 
 
 
