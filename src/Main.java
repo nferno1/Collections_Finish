4afa24ffe728da057b82a39048c07818eec566e6
nferno1 <nferno1@mail.ru>
2022-09-09 15:45:03 +0700

    Cats Example

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
+
+        Cat[] cats = new Cat[4];
+
+        cats[0] = new Cat("Томас");
+        cats[1] = new Cat("Васька");
+        cats[2] = new Cat("Черныш");
+        cats[3] = new Cat("Котяра");
+
+
+        cats[1] = null;
+
+        System.out.println(Arrays.toString(cats));
 
 
 
